c84e3eb127690704b229f646c2c79bff1d9d42fc
pavita1108 <93695808+pavita1108@users.noreply.github.com>
2021-11-19 23:31:07 +0700

    tambah fungsi printmap

--- map.c
+++ map.c
@@ -1,38 +1,40 @@
 #include "map.h"
 #include "mesin_kata.h"
 #include "array.h"
+#include "player.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void readConfig (int *N, char *map, int *MaxRoll, int *Nteleporter, TabInt *teleporter){
-	int i;
+void readConfig (int *N, char *map, int *MaxRoll,  TabInt *teleporter){
+	int i,Nteleporter;
 	MakeEmptyArray(&(*teleporter));
 	STARTKATA();
 	*N = atoi(CKata.TabKata);
 	
 	ADVKATA();
 	strcpy(map,CKata.TabKata);
-	//input nya harus bentuknya char c[100]
 	
 	ADVKATA();
 	*MaxRoll = atoi(CKata.TabKata);
 	
 	ADVKATA();
-	*Nteleporter = atoi(CKata.TabKata);
+	Nteleporter = atoi(CKata.TabKata);
 	
-	(*teleporter).Neff = (*Nteleporter)*2;
-	for (i = 1 ; i <= *Nteleporter ; i++){
+	(*teleporter).Neff = (Nteleporter)*2;
+	for (i = 1 ; i <= Nteleporter ; i++){
 		ADVKATA();
 		(*teleporter).TI[i*2-1] = atoi(CKata.TabKata);
 		
 		ADVKATA();
 		(*teleporter).TI[i*2] = atoi(CKata.TabKata);
 	}
-	// itu yang teleporter kalo yang ganjil itu letak teleporter kalo yang genap petak keluar teleporter
-	//contoh : 3 5 2 9
-	//kalo kena petak 3 maka bakal keluar di 5
-	//kalo kena petak 2 maka bakal keluar di 9
 	
 };
+
+void PrintMap(Player a , char map[100]){
+	printf("%s : ",a.Nama);
+	map[a.CPosition] = '*';
+	printf("%s %d", map,(a.CPosition+1));
+}
 
--- map.h
+++ map.h
@@ -1,5 +1,6 @@
 #include "mesin_kata.h"
 #include "array.h"
+#include "player.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,11 @@
 #ifndef MAP_H
 #define MAP_H
 
-void readConfig (int *N, char *map, int *MaxRoll, int *Nteleporter, TabInt *teleporter);
+
+
+void readConfig (int *N, char *map, int *MaxRoll,  TabInt *teleporter);
+//Membaca file konfigurasi
+void PrintMap(Player a , char map[100]);
+//Mengeluarkan current position dari player
 #endif
 
