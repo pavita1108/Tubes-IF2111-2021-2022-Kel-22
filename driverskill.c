b3ab3ca3afa9bde55b738618f1fefc0ab31bab1b
pavita1108 <93695808+pavita1108@users.noreply.github.com>
2021-11-19 23:29:52 +0700

    ini jd comment biar ga bentrok

--- driverskill.c
+++ driverskill.c
@@ -1,4 +1,4 @@
-#include "listskill.h"
+/*#include "listskill.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,13 +20,13 @@ int main(){
             printf("\nKeterangan:\n1 = Pintu Ga Ke Mana-Mana\n2 = Cermin Pengganda\n3 = Senter Pembesar Hoki\n4 = Senter Pengecil Hoki\n5 = Mesin Penukar Hoki\n");
             printf("\nTekan 0 untuk keluar. Masukkan bilangan negatif untuk membuang skill\n\n"); 
 
-            /* Memasukkan nomor skill yang akan dihapus */
+            // Memasukkan nomor skill yang akan dihapus 
             printf("Masukkan skill: ");
             scanf("%d", &inputskill);  
             DelSkill (&Skill, inputskill);
             if (inputskill > 0){
-                P = SearchList(Buff,inputskill);/*buff tidak bisa distak, cek dulu apakah sudah ada*/
-                if (P = Nil){/*buff belum ada, bisa dimasukkan ke listbuff*/
+                P = SearchList(Buff,inputskill);//buff tidak bisa distak, cek dulu apakah sudah ada
+                if (P = Nil){//buff belum ada, bisa dimasukkan ke listbuff
                     InsVLastList(&Buff, inputskill);//memasukan buff ke dalam list buff
                 }
                 /*jangan lupa saat endturn panggil prosedur DeleteBuff(Buff) untuk membersihkan status buff player*/
@@ -36,3 +36,4 @@ int main(){
 
     
 }
+*/
