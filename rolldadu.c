50309edd275c304bbf9ac5834fc27610f980827c
Vittskuy <88304323+Vittskuy@users.noreply.github.com>
2021-11-20 11:01:45 +0700

    tambahan RollDaduBesar dan RollDaduKecil
    sebelum roll cek apakah player ada buff Senter Pengecil Hoki atau Senter Pembesar Hoki untuk menentukan jenis roll

--- rolldadu.c
+++ rolldadu.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-int rolldadu() {
-    int i, n;
+#include <rolldadu.h>
+#include <time.h>
+#include <player.h>
+#include <map.h>
+
+int RollDaduNormal() {
+    int n;
     srand(time(NULL)); //menggunakan initial time untuk menentukan angka dadu
-    n = (rand() % 6)+ 1; //modulo 6 agar hasil berada di range dadu, +1 agar tidak menghasilkan angka 0
+    n = (rand() % MaxRoll)+ 1; //modulo maxroll agar hasil berada di range dadu, +1 agar tidak menghasilkan angka 0
     return n;
+    //range [1 to MaxRoll]
+}
+
+int RollDaduKecil() {//Didapat dari buff Senter Pengecil Hoki
+    int n;
+    int a = floor(MaxRoll/2);
+    srand(time(NULL));
+    n = (rand() % a)+ 1;
+    return n;
+    //range [1 to floor(MaxRoll/2)]
+}
+
+int RollDaduBesar() {//Didapat dari buff Senter Pembesar Hoki
+    int n;
+    int a= floor(MaxRoll/2);
+    srand(time(NULL)); 
+    n = (rand() % (MaxRoll+1-a) )+ a; 
+    return n;
+    //range [floor(MaxRoll/2) to MaxRoll]
 }
 
 //Test Driver//
 /*
 void main() {
-    printf("%d ", rolldadu());
+    printf("%d ", RollDaduNormal());
 }
 */
 
