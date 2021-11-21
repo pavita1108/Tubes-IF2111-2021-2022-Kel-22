4f854b2bc12e06aa209c2212cb00a3661613d994
Vittskuy <88304323+Vittskuy@users.noreply.github.com>
2021-11-21 15:22:18 +0700

    +roll belum fix
    roll dadu sesuai kondisi buff

--- rolldadu.c
+++ rolldadu.c
@@ -31,7 +31,34 @@ int RollDaduBesar() {//digunakan saat player memiliki buff Senter Pembesar Hoki
     //range [floor(MaxRoll/2) to MaxRoll]
 }
 
-//Test Driver//
+void Roll(Player P){
+//KAMUS LOKAL
+    address Q;
+    int a; //angka dadu yg keluar
+//ALGORITMA
+    Q = SearchList(P.Buff,3); //cek buff sinar pembesar hoki
+    if (Q != Nil){
+        a = RollDaduBesar();
+    }
+    else{//buff  sinar pembesar hoki tidak ditemukan
+        Q = SearchList(P.Buff,4); //cek buff sinar pengecil hoki
+        if (Q != Nil){
+            a = RollDaduKecil();}
+        else{//gapunya buff sinar pengecil hoki juga, roll dadu normal
+            a = RollDaduNormal(); 
+        }
+    }
+    printf("Anda mendapatkan angka:%d", a);
+    //Checking map (belum selesai)
+    /*if (a petak di depan dan di belakang adalah petak terlarang){
+        printf("Borak tidak dapat bergerak");
+    }
+    else if (a petak di depan tidak terlarang tapi a petak belakang terlarang){
+        printf("Anda dapat maju %d", a,"langkah");
+    ()
+*/
+}
+//Test Angka Dadu//
 /*
 void main() {
     printf("%d ", RollDaduNormal());
