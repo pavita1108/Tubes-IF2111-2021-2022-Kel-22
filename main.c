5ab41eed556008f7878057c59a27bce277b4bf64
pavita1108 <93695808+pavita1108@users.noreply.github.com>
2021-11-22 22:44:21 +0700

    udh bisa jalanin roll

--- main.c
+++ main.c
@@ -1,100 +1,77 @@
 #include "player.h"
 #include "map.h"
 #include "array.h"
+#include "command.h"
+#include "listskill.h"
 #include <stdio.h>
+#include <string.h>
 
 
 int main()
 {
 	//Anggep ini player nya ada 2
 	//Bikin player
-	Player player1,player2;
-	MakePlayer(&player1);
-	MakePlayer(&player2);
+	int NPemain;
+	TabPlayer TPlayer;
+	printf("Masukkan banyak pemain : ");
+	scanf("%d",&NPemain);
+	MakeTabPlayer(&TPlayer,NPemain);
 	
 	/*Baca map nya
 	Disini kita udh dapet data map, maxroll,teleporter. 
-	Bisa digunain di main tapi JANGAN UBAH VALUENYA.
-	Masukin ke variabel lain kalo mau pake*/
-	char Map[100];
-	int MaxRoll,N;
-	TabInt Teleporter;
+	Udh di extern(global) jadi bisa dipake dimana aja*/
 	
-	readConfig(&N, &Map,&MaxRoll,&Teleporter);
+	readConfig();
 	
-	/*Mulai game*/
-	PrintMap(player1,Map);
-	printf("\n");
-	PrintMap(player2,Map);
-	printf("\n");
+	//Mulai game
+	puts("-----POSISI AWAL-----");
+	PrintMap(TPlayer,Map);
+
 	
-	/*Mulai Looping*/
-	int HighestPosition,f;
-	HighestPosition = 0 ;
+	//Mulai Looping
 	char command[10];
-	while (HighestPosition != N){
-		//Gameplay buat player 1
-		printf("Masukan command : ");
-		scanf("%s",&command);
-		Command(command,player1,player2,Map,MaxRoll,Teleporter);
-		while(command != "ENDTURN"){
-			printf("Masukan command : ");
+	int MaxPos,a,ronde;
+	ronde = 1;
+	while (MaxPos != N-1){
+		printf("-----RONDE %d----- \n",ronde);
+		int i;
+		for(i = 0; i<TPlayer.Neff; i++){
+			printf("-----PEMAIN ");
+			printf("%s",TPlayer.TI[i].Nama);
+			printf("-----\n");
+			
+			puts("Skill yang didapat : ");
+			a = RandomizeSkill();
+			AddSkill(&(TPlayer.TI[i].Skill),a);
+			
+			printf("Masukan Command : ");
 			scanf("%s",&command);
-			Command(command,player1,player2,Map,MaxRoll,Teleporter);
-			player1.CPosition = f;
-			if (player1.CPosition >= HighestPosition ){
-				HighestPosition = player1.CPosition;
+			while(strcmp(command,"ENDTURN") == 0){
+				puts("Tidak boleh ENDTURN di awal. Masukan command lagi!");
+				printf("Masukan Command : ");
+				scanf("%s",&command);
 			}
-		}
-		
-		//fungsi endturn ditaro disini
-		
-		//Lanjut Gameplay Player2
-		printf("Masukan command : ");
-		scanf("%s",&command);
-		Command(command,player2,player1,Map,MaxRoll,Teleporter);
-		while(command != "ENDTURN"){
-			scanf("%s",&command);
-			printf("Masukan command : ");
-			Command(command,player2,player1,Map,MaxRoll,Teleporter);
-			player1.CPosition = f;
-			if (player2.CPosition >= HighestPosition ){
-				HighestPosition = player2.CPosition;
+			Command(command,&TPlayer,&(TPlayer.TI[i]));
+			if ((TPlayer.TI[i].CPosition)+1 == N){
+				puts("YAY KAMU MENANG. CONGRATSSS!!!!");	
+				exit(1);
+			};
+			
+			while (strcmp(command,"ENDTURN") != 0 ){
+				printf("Masukan Command : ");
+				scanf("%s",&command);
+				Command(command,&TPlayer,&(TPlayer.TI[i]));
+				if ((TPlayer.TI[i].CPosition)+1 == N){
+					puts("YAY KAMU MENANG. CONGRATSSS!!!!");	
+					exit(1);
+				}
 			}
+			ResetPlayer(&(TPlayer.TI[i]));
 		}
-		//fungsi endturn ditaro disini
+		ronde++;
 	}
-	
-	
 };
 
-void Command(char command[10],Player *CPlayer,Player *OtherPlayer,char Map, int MaxRoll,  TabInt Teleporter);
-void Command(char command[10] , Player *CPlayer,Player *OtherPlayer,char Map, int MaxRoll,  TabInt Teleporter){
-	if (command == 'SKILL' ){
-		printf("skill");
-	}
-	else if (command == 'MAP'){
-		PrintMap(*CPlayer,Map);
-		printf("\n");
-		PrintMap(*OtherPlayer,Map);
-		printf("\n");
-	}
-	else if (command == 'BUFF'){
-		printf("buff");
-	}
-	else if (command == 'INSPECT'){
-		printf("inspect");
-	}
-	else if (command == 'ROLL'){
-		printf("roll");
-	}
-	else if (command == 'UNDO'){
-		printf("undo");
-	}
-	else{
-		printf("Tidak Ada Fungsi Tersebut");
-	}
-	
-};
+
 
 
