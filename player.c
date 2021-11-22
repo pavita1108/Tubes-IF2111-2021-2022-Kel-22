d2fe40b90259df5052a251e52072eb6cc272e998
pavita1108 <93695808+pavita1108@users.noreply.github.com>
2021-11-22 21:59:37 +0700

    ganti list buff jadi boolean

--- player.c
+++ player.c
@@ -2,9 +2,20 @@
 #include <stdio.h>
 
 void MakePlayer (Player *P){
-	printf("Nama : ");
 	scanf("%s", &((*P).Nama));
 	(*P).CPosition = 0;
-	CreateEmptyList(&((*P).Buff));
 	CreateEmptyList(&((*P).Skill));
+	(*P).Imun = false;
+	(*P).Pembesar = false;
+	(*P).Pengecil = false;
+	(*P).doneRoll = false;
 };
+
+void MakeTabPlayer (TabPlayer *TPlayer, int neff){
+	int i;
+	(*TPlayer).Neff = neff;
+	for (i=0;i<neff;i++){
+		printf("Masukkan nama player %d : ",(i+1));
+		MakePlayer(&((*TPlayer).TI[i]));
+	}
+}
--- player.h
+++ player.h
@@ -4,15 +4,24 @@
 #include "boolean.h"
 #include "listdp.h"
 
-
 typedef struct {
 	char Nama[100];
 	int CPosition;
 	List Skill;
-	List Buff;
+	boolean Imun;
+	boolean Pembesar;
+	boolean Pengecil;
+	boolean doneRoll;
 }Player;
+
+typedef struct {
+	Player TI[4];
+	int Neff;
+}TabPlayer;
 
 void MakePlayer (Player *P);
 //membuat player baru
 
+void MakeTabPlayer (TabPlayer *TPlayer, int neff);
+//membuat tabplayer
 #endif
