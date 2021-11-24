96f5f57bd494deb71398e9e2c45268e70c33b34d
Vittskuy <88304323+Vittskuy@users.noreply.github.com>
2021-11-24 11:13:18 +0700

    impskill.h

---
+++ impskill.h
@@ -0,0 +1,12 @@
+#ifndef impskill_H
+#define impskill_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "player.h"
+#include "map.h"
+
+void tukarposisi (Player P, TabPlayer TPlayer);
+//menukar posisi seorang player dengan player lain yang dipilih
+ 
+#endif
