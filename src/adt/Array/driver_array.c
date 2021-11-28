eb2b72bf5aeab0a7a90d08a3f23d01965eb2d4eb
meandmyabsurdlife <72054686+meandmyabsurdlife@users.noreply.github.com>
2021-11-28 21:23:13 +0700

    upload driver array

---
+++ src/adt/Array/driver_array.c
@@ -0,0 +1,37 @@
+/* file driver array.c*/
+#include<stdio.h>
+#include "array.h"
+#include "boolean.h"
+
+#include "array.c"
+void BalikIsiTab (TabInt T) {
+/*I.S. Array terdefinisi*/
+/*F.S. Menamplkan elemen dari array*/
+    IdxType i;
+    for (i = T.Neff; i >= IdxMin; i--){
+        printf("%d\n", GetElmtArray (T, i));
+    }
+};
+
+int main() {
+    /*Menerima input jumlah array dan elemennya, kemudian membalik elemen array*/
+    TabInt T;
+    int i, n, elemen;
+
+    MakeEmptyArray(&T);
+
+    printf("Masukkan jumlah elemen : ");
+    scanf("%d", &n);
+    T.Neff = n;
+
+    for (i = IdxMin; i <= n; i++){
+        printf("Masukkan elemen array ke-%d: ", i);
+        scanf("%d", &elemen);
+        T.TI[i] = elemen;
+    }
+
+    printf("Setelah dibalik array menjadi : \n");
+    BalikIsiTab(T);
+
+    return 0;
+}
